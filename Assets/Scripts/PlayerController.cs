4f88702f8fa7d2c66ef2feaf2898ad0a962664a9
pixtonn <82233877+pixtonn@users.noreply.github.com>
2024-05-17 18:30:38 -0600

    Added player movement
    Uses the newer Input System

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public InputAction LeftAction;
+    public float speed;
+    public InputAction moveAction;
     // Start is called before the first frame update
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+    }
+
     void Start()
     {
-        
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Vector2 move = moveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * Time.deltaTime * speed;
+        transform.position = position;
     }
 }
